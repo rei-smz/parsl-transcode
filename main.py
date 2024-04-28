c65df639915b265eb73d691ae09359d83997fe16
Rin Shimizu <52402142+rei-smz@users.noreply.github.com>
2024-04-28 10:39:21 -0400

    Create main function

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import parsl
+from Apps import *
+from Config import transcode_config
 
+with parsl.load(transcode_config):
+    req_str = "{\"path\": \"" + "0" + "\", \"object\": \"video1\", \"args\": {\"format\": \"mkv\"}}"
+    result = transcode(req_str)
+    print(result)
